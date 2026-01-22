43f423da4df30d9d755eca0aeb4874d4fd5f578b
goeka <goeka1337@gmail.com>
2026-01-22 22:25:25 +0900

    feat(mid2): 컬렉션 Set - MyHashSetV1의 실행예제(MyHashSetV1Main)추가

--- src/mid2/collection/set/MyHashSetV1.java
+++ src/mid2/collection/set/MyHashSetV1.java
@@ -1,7 +1,10 @@
 package mid2.collection.set;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
+// mod (%) 연산을 사용한 해시 알고리즘을 사용한 덕분에,
+// 등록, 검색, 삭제 모두 평균 O(1)의 연산속도 가짐.
 public class MyHashSetV1 {
 
     static final int DEFAULT_INITIAL_CAPACITY = 16;
@@ -29,8 +32,8 @@ public class MyHashSetV1 {
 
     public boolean add(int value) {
         int hashIndex = hashIndex(value);
-        LinkedList<Integer> bucket = buckets[hashIndex];
-        if (bucket.contains(value)) {
+        LinkedList<Integer> bucket = buckets[hashIndex]; // 버킷 찾기 -> O(1)
+        if (bucket.contains(value)) { // 버킷 당 데이터가 잘 분산되면, 1,2.. 로 적으므로 O(1)에 가까움.
             return false;
         }
         bucket.add(value);
@@ -40,16 +43,17 @@ public class MyHashSetV1 {
 
     public boolean contains(int searchValue) {
         int hashIndex = hashIndex(searchValue);
-        LinkedList<Integer> bucket = buckets[hashIndex];
+        LinkedList<Integer> bucket = buckets[hashIndex]; // O(1)
 
-        return bucket.contains(searchValue);
+        return bucket.contains(searchValue); // // 버킷 당 데이터가 잘 분산되면, 1,2.. 로 적으므로 O(1)에 가까움.
     }
 
     public boolean remove(int value) {
         int hashIndex = hashIndex(value);
-        LinkedList<Integer> bucket = buckets[hashIndex];
+        LinkedList<Integer> bucket = buckets[hashIndex]; // 버킷 찾기 -> O(1)
         // 리스트에서 remove 메소드는 인덱스기준, value기준 삭제 이렇게 두가지 타입이 있다. 조심해서 의도에 맞는 메소드를 써야 한다.
         // 또한 removed는 삭제 했으면 true, 없어서 삭제 못했으면 false 반환한다.
+        // .remove도 찾아서 제거하는 개념이므로 데이터가 버킷들에 잘 분산되면 .contains 메소드와 같은 O(1)에 가까운 시간복잡도 가진다.
         boolean removed = bucket.remove(Integer.valueOf(value));
         if (removed) {
             size--;
@@ -61,5 +65,14 @@ public class MyHashSetV1 {
 
     private int hashIndex(int value) {
         return value % capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV1{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
     }
 }
